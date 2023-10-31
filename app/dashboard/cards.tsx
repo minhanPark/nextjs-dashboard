d9a7d731417f07303368f03f8bd2059a4c18c5cf
minhanPark <alsgks1218@naver.com>
2023-10-31 17:57:12 +0900

    dynamic 값을 page, layout, route에 주면 정적 렌더링 -> 동적렌더링 등으로 변경 가능하고 페이지 단위에서 loading을 주지않고 컴포넌트 단에서 Suspense를 줘서 스트리밍을 사용할 수 있다.

---
+++ app/dashboard/cards.tsx
@@ -0,0 +1,23 @@
+import { Card } from "../ui/dashboard/cards";
+import { fetchCardData } from "../lib/data";
+
+export default async function Cards() {
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+  return (
+    <>
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card
+        title="Total Customers"
+        value={numberOfCustomers}
+        type="customers"
+      />
+    </>
+  );
+}
